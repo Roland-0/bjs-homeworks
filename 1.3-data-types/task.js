73dae897686ab01277ef392172d6bc8c09ca3c4a
Roland-0 <stomioplus@mail.ru>
2020-11-16 13:35:39 +0300

    Third Homework

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,35 @@
+"use strict"
+
 function calculateTotalMortgage(percent, contribution, amount, date) {
     // код для задачи №1 писать здесь
     // return totalAmount;
+
+    percent = parseInt(percent) / 100;
+    contribution =  parseInt(contribution);
+    amount = parseInt(amount);
+
+   	if (isNaN(percent) == true)
+   		return "Параметр percent содержит неправильное значение " + percent.valueOf();
+   	else if (isNaN(contribution) == true)
+   		return "Параметр contribution содержит неправильное значение " + contribution.valueOf();
+   	else if (isNaN(amount) == true)
+   		return "Параметр amount содержит неправильное значение " + amount.valueOf();//
+
+    let data = date.getFullYear() - new Date().getFullYear();
+    let n = data * 12 - new Date().getMonth() + date.getMonth();
+
+    let month_pay = (amount - contribution) * (percent / 12 + percent / 12 / (((1 + percent / 12) ** n) - 1));
+    let totalAmount = month_pay * n;
+
+    return Number(totalAmount.toFixed(2));
 }
 
 function getGreeting(name) {
     // код для задачи №2 писать здесь
     // return greeting;
+
+    if (!name || name == undefined)
+    	return "Привет, мир! Меня зовут Аноним.";
+    else
+    	return `Привет, мир! Меня зовут ${name}.`;
 }
